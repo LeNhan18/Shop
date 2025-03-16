c7e86552dd78798e6e0aa9f607e1cfec4b376aeb
LeThanhNhan <lnhan8132@gmail.com>
2025-03-16 21:12:00 +0700

    Add Login And Register(Create User)

--- Controller/UserController.java
+++ Controller/UserController.java
@@ -3,6 +3,7 @@ package com.project.shopapp.Controller;
 
 import com.project.shopapp.DTOS.UserDTO;
 import com.project.shopapp.DTOS.UserLoginDTO;
+import com.project.shopapp.Service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/register")
-    public ResponseEntity<?> register (@Valid @RequestBody UserDTO userDTO
+    public ResponseEntity<?> CreateUser (@Valid @RequestBody UserDTO userDTO
             ,BindingResult result) {
         try{
             if(result.hasErrors()){
@@ -25,9 +32,10 @@ public class UserController {
                         .toList();
                 return ResponseEntity.badRequest().body(errorMessages);
             }
-            if(userDTO.getPassword().equals(userDTO.getRetypePassword())){
+            if(!userDTO.getPassword().equals(userDTO.getRetypePassword())){
                 return ResponseEntity.badRequest().body("Password does not match");
             }
+            userService.createUser(userDTO);
             return ResponseEntity.ok("Register successfly!");
 
         }catch(Exception e){
@@ -37,8 +45,14 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> login (@Valid @RequestBody UserLoginDTO userloginDTO){
         //Kiểm tra thông tin đăng nhập và sinh tokens
+        try{
+            String token = userService.Login(userloginDTO.getPhoneNumber(), userloginDTO.getPassword());
+            return ResponseEntity.ok(token);
+
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
         //trả token về reponse
-        return ResponseEntity.ok("Login successfly!");
 
     }
 //    @GetMapping("")
