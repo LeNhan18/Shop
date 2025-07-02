bcf51991f1f609e9575225d7c7fc666c811d6da6
LeThanhNhan <lnhan8132@gmail.com>
2025-07-02 23:14:07 +0700

    cấu hình filter này vào chuỗi filter của Spring Security và chỉ cho phép /users/login, /users/register là public, các API khác phải xác thực JWT

--- Configurations/WebSecurityConfig.java
+++ Configurations/WebSecurityConfig.java
@@ -7,20 +7,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import com.project.shopapp.Security.CustomSecurityFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final CustomSecurityFilter customSecurityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> {
-                    requests.requestMatchers("*/**")
-                            .permitAll()
-                            .anyRequest()
-                            .authenticated();
-                });
+                    requests.requestMatchers("/users/login", "/users/register").permitAll();
+                    requests.anyRequest().authenticated();
+                })
+                .addFilterBefore(customSecurityFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
